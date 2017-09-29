f83e62ed675a0a6558b1dbc0036a127dde6f66b1
Christie Grinham <christiegrinham@gmail.com>
2017-09-29 11:22:17 +0100

    remove and change secret key

--- takefive/settings.py
+++ takefive/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import emailpassword
+import djangosecrets
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3$^cd+=$&%nq3@g=yq@c&32x6m)*83u%8r9x1y)3iux*$-iig8'
+SECRET_KEY = djangosecrets.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
